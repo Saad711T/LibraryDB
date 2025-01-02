371133b567c470e9fcac90afcbe48199fec5312a
0xSaad <75455552+Saad711T@users.noreply.github.com>
2025-01-02 14:04:25 +0300

    Add queries (DML)

---
+++ sql_queries.sql
@@ -0,0 +1,21 @@
+-- استعلام عن أكثر الكتب تحتوي على نسخ
+SELECT title, available_copies
+FROM books
+ORDER BY available_copies DESC
+LIMIT 5;
+
+-- استعلام عن المؤلفين الذين ولدوا بعد عام 1900
+SELECT name, birth_year
+FROM authors
+WHERE birth_year > 1900;
+
+-- استعلام عن عدد الكتب لكل تصنيف
+SELECT genre, COUNT(*) AS total_books
+FROM books
+GROUP BY genre
+ORDER BY total_books DESC;
+
+-- استعلام عن الكتب المنشورة خلال القرن العشرين
+SELECT title, publication_year
+FROM books
+WHERE publication_year BETWEEN 1900 AND 2000;
